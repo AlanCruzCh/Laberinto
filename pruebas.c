742f73ab9827e4568cce40e14ed1c8884bc9e133
AlanCruzCh <alan.francisco.cruz.chavez.17@gmail.com>
2022-03-02 15:50:01 -0600

    Haciendo cambios a las funciones derecha y izquierda

--- pruebas.c
+++ pruebas.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *avanza_derecha(int laberinto_aux[][7], int fila, int columna, int posicion_columna, int posicion_fila_columna_bandera[]){
+int *avanza_derecha(int laberinto_aux[][7], int fila, int columna, int posicion_fila_columna_bandera[]){
     if ((laberinto_aux[fila + 1][columna] != 1) && (fila != 7)){
         laberinto_aux[fila][columna] = 2;
         posicion_fila_columna_bandera[0] = (fila += 1);
-        posicion_fila_columna_bandera[1] = posicion_columna;
+        posicion_fila_columna_bandera[1] = columna;
         posicion_fila_columna_bandera[2] = 2;  /*2 es la bandera para que baje */
         return posicion_fila_columna_bandera;
     }
     else if ((laberinto_aux[fila - 1][columna] != 1) && (fila != 0)){
         laberinto_aux[fila][columna] = 2;
         posicion_fila_columna_bandera[0] = (fila -= 1); 
-        posicion_fila_columna_bandera[1]= posicion_columna;
+        posicion_fila_columna_bandera[1] = columna;
         posicion_fila_columna_bandera[2] = 3; /* 3 es la bandera para que suba */
         return posicion_fila_columna_bandera;
     }
-    else if ((laberinto_aux[fila][columna + 1] == 1)|| (columna == 7)){
-        posicion_fila_columna_bandera[1] = posicion_columna;
+    else if ((laberinto_aux[fila][columna + 1] == 1) || (columna == 7)){
+        posicion_fila_columna_bandera[1] = columna;
         laberinto_aux[fila][columna] = 2;
         if ((laberinto_aux[fila + 1][columna] != 1) && (fila != 7)){
             posicion_fila_columna_bandera[0] = (fila +=1);
@@ -29,7 +29,7 @@ int *avanza_derecha(int laberinto_aux[][7], int fila, int columna, int posicion_
             posicion_fila_columna_bandera[2] = 1; /* 1 es la bandera para que valla a la izquierda */
             return posicion_fila_columna_bandera;
         }    
-        else{
+        else if((laberinto_aux[fila -1][columna] != 1) && (fila != 0)){
             posicion_fila_columna_bandera[0] = (fila -= 1);
             posicion_fila_columna_bandera[2] = 3; /* 3 es la bandera para que suba */
             return posicion_fila_columna_bandera;
@@ -37,28 +37,27 @@ int *avanza_derecha(int laberinto_aux[][7], int fila, int columna, int posicion_
     }
     else{
         laberinto_aux[fila][columna] = 2;
-        posicion_columna += 1;
     }
-    return avanza_derecha(laberinto_aux,fila,columna + 1, posicion_columna, posicion_fila_columna_bandera);
+    return avanza_derecha(laberinto_aux,fila,columna + 1, posicion_fila_columna_bandera);
 }
 
-int *avanza_izquierda(int laberinto_aux[][7], int fila, int columna, int posicion_columna, int posicion_fila_columna_bandera[]){
+int *avanza_izquierda(int laberinto_aux[][7], int fila, int columna, int posicion_fila_columna_bandera[]){
     if ((laberinto_aux[fila + 1][columna] != 1) && (fila != 7)){
         laberinto_aux[fila][columna] = 2;
         posicion_fila_columna_bandera[0] = (fila += 1);
-        posicion_fila_columna_bandera[1] = posicion_columna;
+        posicion_fila_columna_bandera[1] = columna;
         posicion_fila_columna_bandera[2] = 2;  /*2 es la bandera para que baje */
         return posicion_fila_columna_bandera;
     }
     else if ((laberinto_aux[fila - 1][columna] != 1) && (fila != 0)){
         laberinto_aux[fila][columna] = 2;
         posicion_fila_columna_bandera[0] = (fila -= 1); 
-        posicion_fila_columna_bandera[1]= posicion_columna;
+        posicion_fila_columna_bandera[1]= columna;
         posicion_fila_columna_bandera[2] = 3; /* 3 es la bandera para que suba */
         return posicion_fila_columna_bandera;
     }
     else if ((laberinto_aux[fila][columna - 1] == 1)|| (columna == 0)){
-        posicion_fila_columna_bandera[1] = posicion_columna;
+        posicion_fila_columna_bandera[1] = columna;
         laberinto_aux[fila][columna] = 2;
         if ((laberinto_aux[fila + 1][columna] != 1) && (fila != 7)){
             posicion_fila_columna_bandera[0] = (fila +=1);
@@ -70,7 +69,7 @@ int *avanza_izquierda(int laberinto_aux[][7], int fila, int columna, int posicio
             posicion_fila_columna_bandera[2] = 0; /* 0 es la bandera para que valla a la derecha */
             return posicion_fila_columna_bandera;
         }    
-        else{
+        else if ((laberinto_aux[fila - 1][columna] != 1) && (fila != 0)){
             posicion_fila_columna_bandera[0] = (fila -= 1);
             posicion_fila_columna_bandera[2] = 3; /* 3 es la bandera para que suba */
             return posicion_fila_columna_bandera;
@@ -78,14 +77,14 @@ int *avanza_izquierda(int laberinto_aux[][7], int fila, int columna, int posicio
     }
     else{
         laberinto_aux[fila][columna] = 2;
-        posicion_columna -= 1;
     }
-    return avanza_izquierda(laberinto_aux,fila,columna - 1, posicion_columna, posicion_fila_columna_bandera);
+    return avanza_izquierda(laberinto_aux,fila,columna - 1, posicion_fila_columna_bandera);
 }
 
 
 
 int main(){
+    /*
     int laberinto[7][7] = { {0,1,1,1,1,1,1},
                             {0,0,0,0,1,1,1},
                             {1,0,1,1,1,1,1},
@@ -93,14 +92,33 @@ int main(){
                             {1,0,1,1,1,0,1},
                             {0,0,0,0,0,0,0},
                             {0,1,0,0,1,0,0}};
+                            */
+    int laberinto[7][7] = { {0,0,0,0,0,0,0},
+                            {1,1,1,1,1,1,1},
+                            {1,0,1,1,1,1,1},
+                            {0,0,1,1,1,1,1},
+                            {1,0,1,1,1,0,1},
+                            {0,0,0,0,0,0,0},
+                            {0,1,0,0,1,0,0}};
+    
     int posicion_fila_columna_bandera[3] = {0,0,0};
     int fila = 0; 
     int columna = 0;  
     int bandera = 0;
-    int pos_columna = 0;
-    int pos_fila = 0;
     int *derecha, *izquierda, *abajo, *arriba; 
-    
+    derecha = avanza_derecha(laberinto, fila, columna, posicion_fila_columna_bandera);
+    fila = derecha[0];
+    columna = derecha[1];
+    bandera = derecha[2];    
+    printf("%d %d %d\n\n", fila, columna, bandera);
+
+    izquierda = avanza_izquierda(laberinto, fila, columna, posicion_fila_columna_bandera);
+    fila = izquierda[0];
+    columna = izquierda[1];
+    bandera = izquierda[2];    
+    printf("%d %d %d\n\n", fila, columna, bandera);
+                    
+    /*
     while((fila != 2)&&(columna != 2)){
         switch (bandera)
         {
@@ -136,5 +154,6 @@ int main(){
         }
         printf("\n");
     }
+    */
    return 0;
 }
